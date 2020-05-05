c7618a29e359c23d8e94d3e758005faea1eb86a5
Chris Clark <chris.clark876@gmail.com>
2020-05-05 19:21:34 +0100

    Remove dead code

--- WebExample/src/App.vue
+++ WebExample/src/App.vue
@@ -11,15 +11,8 @@
         <v-spacer></v-spacer>
       </v-app-bar>
       <v-content class="fill-height" fluid>
-        <v-card width="400" style="top: 400% !important;" class="mx-auto">
-          <!-- <v-card-title class="pb-0">
-            Sign In
-            <hr />
-          </v-card-title> -->
+        <v-card width="400" style="top: 400% !important;" class="mx-auto">          
           <v-card-text>
-            <!-- <v-form> -->
-            <!-- <v-card> -->
-              <!-- <v-card-text> -->
                 <v-btn
                 style="left: 8%"
                   :loading="loading3"
@@ -31,31 +24,9 @@
                 <v-icon left dark>mdi-google</v-icon>
                   Sign In With Google
                   
-                </v-btn>
-              <!-- </v-card-text> -->
-            <!-- </v-card> -->
-            <!-- <v-text-field
-                                
-                                
-                          
-                                label="Email"
-                                prepend-icon="mdi-email"
-                        />
-                        <v-text-field
-                                
-                                type="password"
-                                label="Password"
-                                prepend-icon="mdi-lock"
-            />-->
-            <!-- </v-form> -->
-            <v-alert v-if="signinError" dense type="error">Details are incorrect</v-alert>
-            <!-- <p style="font-size: 14px" @click="$emit('changeSignUp', 'signUp')" class="ma-0 pa-0 toggle">Need an -->
-            <!-- account? Sign Up</p> -->
+                </v-btn>        
+            <v-alert v-if="signinError" dense type="error">Details are incorrect</v-alert>            
           </v-card-text>
-          <!-- <v-divider></v-divider> -->
-          <!-- <v-card-actions> -->
-          <!-- <v-btn @click="login" dark color="grey">Login</v-btn> -->
-          <!-- </v-card-actions> -->
         </v-card>
       </v-content>
     </v-container>
