5b412c33d1c9624f516f1b0e66984182f92ba654
Chris Clark <chris.clark876@gmail.com>
2020-05-08 17:39:18 +0100

    barebones app skeleton screen

--- App/App.tsx
+++ App/App.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
+import {Provider as PaperProvider, Button, Card, Appbar} from 'react-native-paper';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+    return (
+        <PaperProvider>
+            <Appbar style={{top: '8%', backgroundColor: 'red'}}>
+        <Appbar.Action icon="menu" onPress={() => console.log('Pressed archive')} />        
+      </Appbar>
+            <View style={styles.container}>
+                
+                <Card elevation={100} style={{height: '30%', marginLeft: '5%', marginRight: '5%'}}>
+                <Image style={{height: 200, width: 200, left: '24%'}}
+    
+        source={{uri: 'https://cdn.discordapp.com/attachments/707595841673756712/707604337362468894/RevolveYC.PNG'}}
+      />
+                    <Card.Content>
+                <Button color="red" style={{top: '0%'}} icon="google" mode="contained" onPress={() => console.log('Pressed')}>
+                    Sign In With Google
+                </Button>
+                    </Card.Content>
+                    </Card>
+            </View>
+        </PaperProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        // flex: 1,
+        // backgroundColor: '#fff',
+        // alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
